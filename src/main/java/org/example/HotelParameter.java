6546c88ab0519845b9e64e33d46a29b205793f4b
Rihal Shaikh <rihalshaikh999@gmail.com>
2023-12-28 23:32:39 +0530

    Added class and functions to print the cheapest hotel in given date range

--- src/main/java/org/example/HotelParameter.java
+++ src/main/java/org/example/HotelParameter.java
@@ -1,28 +1,76 @@
 package org.example;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Date;
 public class HotelParameter {
     public String hotelName;
     public Double price;
-    public HotelParameter(String hotelName, Double price) {
+    public Date startDate;
+    public Date endDate;
+    public HotelParameter(String hotelName, Double price, Date startDate, Date endDate) {
         this.hotelName = hotelName;
         this.price = price;
+        this.startDate=startDate;
+        this.endDate=endDate;
     }
-    public static HotelParameter createHotel(String hotelName, Double price) {
-        return new HotelParameter(hotelName, price);
+    public static HotelParameter createHotel(String hotelName, Double price,Date startDate, Date endDate) {
+        return new HotelParameter(hotelName, price, startDate, endDate);
+    }
+    public static HotelParameter cheapHotel(HashMap<String, HotelParameter>hm)
+    {
+        HotelParameter cheap=null;
+        for(HotelParameter hotel:hm.values())
+        {
+            if(cheap==null||hotel.price<cheap.price)
+                cheap=hotel;
+        }
+        return cheap;
     }
     public static void main(String[] args) {
-        HotelParameter ob1 = createHotel("Lakewood", 110.0);
-        HotelParameter ob2 = createHotel("Bridgewood", 160.0);
-        HotelParameter ob3 = createHotel("Ridgewood", 210.0);
+        HotelParameter ob1 = createHotel("Lakewood", 110.0, parseDate("10/sep/2020"), parseDate("11/sep/2020"));
+        HotelParameter ob2 = createHotel("Bridgewood", 160.0,parseDate("10/sep/2020"), parseDate("11/sep/2020"));
+        HotelParameter ob3 = createHotel("Ridgewood", 210.0,parseDate("10/sep/2020"), parseDate("11/sep/2020"));
         HashMap<String, HotelParameter> hm = new HashMap<>();
         hm.put(ob1.hotelName, ob1);
         hm.put(ob2.hotelName, ob2);
         hm.put(ob3.hotelName, ob3);
-        for (Map.Entry<String, HotelParameter> entry : hm.entrySet()) {
-            String key = entry.getKey();
-            HotelParameter ob = entry.getValue();
-            System.out.println(ob.hotelName + " " + ob.price);
+//        for (Map.Entry<String, HotelParameter> entry : hm.entrySet()) {
+//            HotelParameter ob = entry.getValue();
+//            System.out.println(ob.hotelName + " " + ob.price + " Start Date: " + ob.startDate + " End Date: " + ob.endDate);
+//        }
+
+        Date rangeStartDate = parseDate("10/sep/2020");
+        Date rangeEndDate = parseDate("11/sep/2020");
+        showHotelByDateRange(hm, rangeStartDate, rangeEndDate);
+        HotelParameter cheap=cheapHotel(hm);
+        if(cheap!=null)
+            System.out.println("Cheap Hotel: "+cheap.hotelName +" "+cheap.price);
+        else
+            System.out.println("No Hotels Found...!!!");
+    }
+
+    public static void showHotelByDateRange(HashMap<String, HotelParameter> hm, Date startDate, Date endDate) {
+        for (HotelParameter hotel : hm.values()) {
+            if (isDateRangeOverlap(startDate, endDate, hotel.startDate, hotel.endDate)) {
+                System.out.println("Range: " + startDate + " - " + endDate + "_" + hotel.hotelName + "_" + hotel.price);
+            }
         }
     }
+    public static boolean isDateRangeOverlap(Date targetStartDate, Date targetEndDate, Date startDate, Date endDate) {
+        return (targetStartDate.compareTo(endDate) <= 0 && targetEndDate.compareTo(startDate) >= 0);
+    }
+    public static Date parseDate(String dateString) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy");
+        try {
+            return sdf.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+//    public static boolean isDateInRange(Date targetDate, Date startDate, Date endDate) {
+//        return targetDate.compareTo(startDate) >= 0 && targetDate.compareTo(endDate) <= 0;
+//    }
 }
--- src/test/java/TestHotelReservation.java
+++ src/test/java/TestHotelReservation.java
@@ -1,24 +1,56 @@
 import junit.framework.Assert;
 import org.example.HotelParameter;
 import org.junit.Test;
+
+import static org.example.HotelParameter.parseDate;
+import static org.junit.Assert.assertEquals;
+
 public class TestHotelReservation
 {
     @Test
     public void UC1_1() {
-        HotelParameter ob=new HotelParameter("Lakewood",110.0);//actual
+        HotelParameter ob=new HotelParameter("Lakewood",110.0,parseDate("10/sep/2020"), parseDate("11/sep/2020"));//actual
         Assert.assertEquals("Lakewood", ob.hotelName);
         Assert.assertEquals(110.0, ob.price);
     }
     @Test
     public void UC1_2() {
-        HotelParameter ob=new HotelParameter("Bridgewood", 160.0);//actual
+        HotelParameter ob=new HotelParameter("Bridgewood", 160.0, parseDate("10/sep/2020"), parseDate("11/sep/2020"));//actual
         Assert.assertEquals("Bridgewood", ob.hotelName);
         Assert.assertEquals(160.0, ob.price);
     }
     @Test
     public void UC1_3() {
-        HotelParameter ob1=new HotelParameter("Ridgewood", 210.0);//actual
+        HotelParameter ob1=new HotelParameter("Ridgewood", 210.0, parseDate("10/sep/2020"), parseDate("11/sep/2020"));//actual
         Assert.assertEquals("Ridgewood", ob1.hotelName);
         Assert.assertEquals(210.0, ob1.price);
+    }
+    @Test
+    public void UC2_1()
+    {
+        HotelParameter ob1=new HotelParameter("Lakewood",110.0,parseDate("10/sep/2020"),parseDate("11/sep/2020"));//actual
+        HotelParameter ob2=new HotelParameter("Bridgewood", 160.0,parseDate("10/sep/2020"),parseDate("11/sep/2020"));//actual
+        HotelParameter ob3=new HotelParameter("Ridgewood", 210.0,parseDate("10/sep/2020"),parseDate("11/sep/2020"));//actual
+        if (ob1.price<ob2.price&&ob1.price<ob3.price) {
+            Assert.assertEquals("Lakewood", ob1.hotelName);
+            Assert.assertEquals(110.0, ob1.price);
+//            Assert.assertEquals("Thu Sep 10 00:00:00 IST 2020", ob1.startDate);
+//            Assert.assertEquals("11/sep/2020", ob1.endDate);
+            System.out.println("Hotel "+ob1.hotelName +" is Cheapest at Price: "+ob1.price+" Available from:- "+ob1.startDate+ " to "+ob1.endDate);
+        }
+        else if (ob2.price<ob1.price&&ob2.price<ob3.price) {
+            Assert.assertEquals("Bridgewood", ob2.hotelName);
+            Assert.assertEquals(160.0, ob2.price);
+            Assert.assertEquals("Thu Sep 10 00:00:00 IST 2020", ob2.startDate);
+            Assert.assertEquals("11/sep/2020", ob2.endDate);
+            System.out.println("Hotel "+ob2.hotelName +" is Cheapest at Price: "+ob2.price+" Available from:- "+ob2.startDate+ " to "+ob2.endDate);
+        }
+        else if (ob3.price<ob1.price&&ob3.price<ob2.price) {
+            Assert.assertEquals("Ridgewood", ob3.hotelName);
+            Assert.assertEquals(210.0, ob3.price);
+            Assert.assertEquals("Thu Sep 10 00:00:00 IST 2020", ob3.startDate);
+            Assert.assertEquals("11/sep/2020", ob3.endDate);
+            System.out.println("Hotel "+ob3.hotelName +" is Cheapest at Price: "+ob3.price+" Available from:- "+ob3.startDate+ " to "+ob3.endDate);
+        }
     }
 }
